130375127188e52567034af50ba4ade8c3679302
Guilherme Arcencio <guiarcencio@hotmail.com>
2023-10-13 12:52:03 -0300

    satisfaz passo 3

--- estado_irc.py
+++ estado_irc.py
@@ -1,13 +1,22 @@
+from asyncio import Lock
+
 # Singleton de dados do servidor
 class EstadoIRC:
     _instancia = None
+    _mutex = Lock()
 
     @classmethod
-    def obter(cls):
+    async def obter(cls):
+        await cls._mutex.acquire()
+
         if cls._instancia is None:
             cls._instancia = EstadoIRC()
         
         return cls._instancia
+    
+    @classmethod
+    def liberar(cls):
+        cls._mutex.release()
     
     def __init__(self):
         self._apelidos_usados = set()
--- mensagens.py
+++ mensagens.py
@@ -1,11 +1,28 @@
-from grader.tcp import Conexao
+import re
 
-def interpretar_mensagem(conexao: Conexao, msg: str):
-    campos = msg.strip(' \r\n').split(' ')
+from grader.tcp import Conexao
+from estado_irc import EstadoIRC
+
+def validar_nome(nome: bytes) -> bool:
+    return re.match(br'^[a-zA-Z][a-zA-Z0-9_-]*$', nome) is not None
+
+def interpretar_mensagem(conexao: Conexao, msg: bytes):
+    campos = msg.strip(b' \r\n').split(b' ')
     if len(campos) < 2: return
 
-    if campos[0] == 'PING':
-        tratar_ping(conexao, ' '.join(campos[1:]))
+    verbo = campos[0].upper()
+    if verbo == b'PING':
+        tratar_ping(conexao, b' '.join(campos[1:]))
+    elif verbo == b'NICK':
+        tratar_nick(conexao, b' '.join(campos[1:]))
 
-def tratar_ping(conexao: Conexao, payload: str):
-    conexao.enviar(f':server PONG server :{payload}\r\n'.encode('utf-8'))
+def tratar_ping(conexao: Conexao, payload: bytes):
+    conexao.enviar(b':server PONG server :%s\r\n' % payload)
+
+def tratar_nick(conexao: Conexao, apelido: bytes):
+    if not validar_nome(apelido):
+        conexao.enviar(b':server 432 * %s :Erroneous nickname\r\n' % apelido)
+        return
+    
+    conexao.enviar(b':server 001 %s :Welcome\r\n' % apelido)
+    conexao.enviar(b':server 422 %s :MOTD File is missing\r\n' % apelido)
